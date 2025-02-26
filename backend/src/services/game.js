63a254adb63d16a6a2bfe52ce09205d5f5adeb6e
SVelikiy <108265367+SVelikiy@users.noreply.github.com>
2025-02-26 15:39:38 +0200

    fix process round

--- backend/src/services/game.js
+++ backend/src/services/game.js
@@ -1,9 +1,16 @@
-import {BattleLogCollection} from '../db/models/battleLog.js';
-import { RoomsCollection } from '../db/models/room.js';
+import { BattleLogCollection } from "../db/models/battleLog.js";
+import { RoomsCollection } from "../db/models/room.js";
 
-export const processRound = async (roomId, player1, player2, action1, action2) => {
-  const damage1 = action1.attack_zone !== action2.block_zone ? 1 : 0;
-  const damage2 = action2.attack_zone !== action1.block_zone ? 1 : 0;
+exports.processRound = async (roomId, player1, player2, action1, action2) => {
+  const room = await RoomsCollection.findById(roomId);
+  if (!room) return null;
+
+  if (!room.hp) {
+    room.hp = { [player1]: 10, [player2]: 10 };
+  }
+
+  let damage1 = action1.attack_zone !== action2.block_zone ? 1 : 0;
+  let damage2 = action2.attack_zone !== action1.block_zone ? 1 : 0;
 
   await BattleLogCollection.create({
     room_id: roomId,
@@ -21,20 +28,17 @@ export const processRound = async (roomId, player1, player2, action1, action2) =
     damage: damage1,
   });
 
-  const room = await RoomsCollection.findById(roomId);
-  if (!room) return null;
-
-  room.hp = room.hp || {};
   room.hp[player1] = (room.hp[player1] || 10) - damage1;
   room.hp[player2] = (room.hp[player2] || 10) - damage2;
 
   if (room.hp[player1] <= 0 && room.hp[player2] <= 0) {
-    room.status = 'finished';
+    room.status = "finished";
+    room.winner = null;
   } else if (room.hp[player1] <= 0) {
-    room.status = 'finished';
+    room.status = "finished";
     room.winner = player2;
   } else if (room.hp[player2] <= 0) {
-    room.status = 'finished';
+    room.status = "finished";
     room.winner = player1;
   }
 
